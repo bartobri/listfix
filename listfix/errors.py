c75eb41aa6a40e13bdcdc38a111128452c3c633e
Brian Barto <bartobrian@gmail.com>
2022-04-01 16:10:35 -0400

    Add logging to error module.
    	modified:   listfix/errors.py

--- listfix/errors.py
+++ listfix/errors.py
@@ -1,16 +1,19 @@
 import sys
+from .log import Log
 
 class Errors:
 
 	def __init__(self, debug=False):
 		self.debug = debug
 		self._exception_handler = None
+		self.log = Log()
 
 	def set_exception_handler(self):
 		self._exception_handler = sys.excepthook
 		sys.excepthook = self.exception_handler
 
 	def exception_handler(self, exception_type, exception, traceback):
+		self.log.error(f"{exception_type.__name__}, {exception}")
 		if (self.debug):
 			self._exception_handler(exception_type, exception, traceback)
 		else:
