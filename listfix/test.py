be47437803bfb1bb48f440b88c34e83ccd305873
Brian Barto <bartobrian@gmail.com>
2022-04-14 18:19:05 -0400

    Convert tests for new json database
    	modified:   listfix/test.py

--- listfix/test.py
+++ listfix/test.py
@@ -14,7 +14,7 @@ class Test(unittest.TestCase):
     def setUp(self):
 
         ## db setup
-        self.db_name = "/tmp/test.sqlite3"
+        self.db_name = "/tmp/test.json"
         if (exists(self.db_name)):
             os.remove(self.db_name)
 
@@ -25,7 +25,6 @@ class Test(unittest.TestCase):
 
     def test_db(self):
 
-        list_id = None
         list_name = "List Name"
         list_email = "test@test.com"
         recipient_name = "Recipient Name"
@@ -33,21 +32,18 @@ class Test(unittest.TestCase):
 
         ## init
         db = DB(self.db_name)
-        self.assertTrue(type(db) is listfix.db.DB)
+
+        ## save()
+        db.save()
         self.assertTrue(exists(self.db_name))
 
         ## create_list()
         db.create_list(list_email, list_name)
-        row = db.db.execute("select id, name, email from lists").fetchone()
-        list_id = row[0]
-        self.assertEqual(row[1], list_name)
-        self.assertEqual(row[2], list_email)
+        self.assertIn(list_email, db.json.keys())
+        self.assertEqual(list_name, db.json[list_email]['name'])
 
         ## check_list_exists()
         self.assertTrue(db.check_list_exists(list_email))
-
-        ## get_list_id()
-        self.assertEqual(list_id, db.get_list_id(list_email))
 
         ## get_list_name()
         self.assertEqual(list_name, db.get_list_name(list_email))
@@ -57,17 +53,11 @@ class Test(unittest.TestCase):
 
         ## create_recipient()
         db.create_recipient(list_email, recipient_email, recipient_name)
-        row = db.db.execute("select id, list_id, name, email from recipients").fetchone()
-        recipient_id = row[0]
-        self.assertEqual(row[1], list_id)
-        self.assertEqual(row[2], recipient_name)
-        self.assertEqual(row[3], recipient_email)
+        self.assertIn(recipient_email, db.json[list_email]['recipients'].keys())
+        self.assertEqual(recipient_name, db.json[list_email]['recipients'][recipient_email]['name'])
 
         ## check_recipient_exists()
         self.assertTrue(db.check_recipient_exists(list_email, recipient_email))
-
-        ## get_recipient_id()
-        self.assertEqual(recipient_id, db.get_recipient_id(list_email, recipient_email))
 
         ## get_recipient_name()
         self.assertEqual(recipient_name, db.get_recipient_name(list_email, recipient_email))
@@ -83,8 +73,6 @@ class Test(unittest.TestCase):
         db.destroy_list(list_email)
         self.assertFalse(db.check_list_exists(list_email))
 
-        ## close
-        db.close()
         os.remove(self.db_name)
 
     def test_log(self):
